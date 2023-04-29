92791c341cb42e04f9255cf5488ccb92a21229a8
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-29 13:50:43 +0200

    Add files via upload

---
+++ IF statements/index.js
@@ -0,0 +1,24 @@
+let age = window.prompt("enter your age");
+
+if(age >= 18){
+    console.log("You are an adult");
+}
+else if(age < 0){
+    console.log("WTF");
+}
+else{
+    console.log("You are a child");
+}
+
+/*we can also use bool
+
+let online = true //it can be false
+if(online){
+    console.log("you are online");
+}
+else{
+    console.log("you are offline");
+}
+
+
+*/
