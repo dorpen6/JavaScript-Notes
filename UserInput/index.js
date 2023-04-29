4dd3f8c026de8178d72b328cac011040017eca67
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-29 13:53:48 +0200

    Add files via upload

---
+++ UserInput/index.js
@@ -0,0 +1,5 @@
+document.getElementById("myButton").onclick = function(){
+	username = document.getElementById("myText").value;
+	console.log(username)
+	document.getElementById("myLabel").innerHTML = "hello " + username;
+}
