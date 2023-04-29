154d9d3172bd734c3d412a24007181e90614355a
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-29 16:42:01 +0200

    Add files via upload

---
+++ Break && Continue/index.js
@@ -0,0 +1,17 @@
+for(let i = 1; i <= 20; i+=1)
+{
+    if(i ==13)
+    {
+        break; //this will break the loop on 13
+    }
+    console.log(i);
+}
+
+for(let a = 1; a <= 20; a+=1)
+{
+    if(a ==13)
+    {
+        continue; //this will skip 13
+    }
+    console.log(a);
+}
