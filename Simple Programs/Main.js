00715fed51629167a9fefa8a0183ba40f2086868
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-29 13:18:50 +0200

    Delete Simple Programs directory

--- Simple Programs/Main.js
+++
@@ -1,9 +0,0 @@
-let fullName = window.prompt("Full name?");
-let firstName;
-let lastName;
-
-firstName = fullName.slice(0, fullName.indexOf(" "));
-lastName = fullName.slice(fullName.indexOf(" ") +1);
-
-console.log(firstName);
-console.log(lastName);
