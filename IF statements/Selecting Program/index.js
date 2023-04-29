32c9d0c0d70400f4be3633787539ce01c0930807
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-29 14:20:39 +0200

    Add files via upload

---
+++ IF statements/Selecting Program/index.js
@@ -0,0 +1,14 @@
+
+document.getElementById("myButton").onclick = function(){    //so when we click the button, the program will do:
+    if(document.getElementById("myCheckBox").checked)
+    {
+        console.log("Selected");
+
+
+    }
+    else
+    {
+        console.log("Not Selected");
+
+    }    
+}
