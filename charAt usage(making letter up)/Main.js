144eb9d05d4e0deb5e86f92f138c581b41290d2c
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-29 13:31:17 +0200

    Add files via upload

---
+++ charAt usage(making letter up)/Main.js
@@ -0,0 +1,6 @@
+let userName = window.prompt("Enter your username");
+
+let letter = userName.charAt(0).toUpperCase().trim(); //we called the first letter of the username(0) by using "charAt" 
+
+console.log(letter);
+console.log(userName);
