4f17e6b1e88c20fd926fb5f9f33a37f256df3c20
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-30 11:27:42 +0200

    Add files via upload

---
+++ arrays/arrays/index.js
@@ -0,0 +1,21 @@
+// array = like a variable that can stores multiple values
+
+let fruits = ["apple", "orange", "banana"];
+
+fruits[2] = "cocnut"; //changes the 2nd element
+fruits.push("Lemon"); //adds an element to the end
+fruits.unshift("mango");// adds an element to the beginning
+fruits.pop(); //removes the last element
+fruits.shift(); //removes elemts from beginning
+
+
+
+
+let length = fruits.length;
+let index = fruits.indexOf("Orange");
+
+console.log(index);
+
+
+
+console.log(fruits);
