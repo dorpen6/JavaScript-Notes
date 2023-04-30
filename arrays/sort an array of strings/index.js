318a857e31623648502e0972f632a48f10542c43
Ali <108894573+wenranne@users.noreply.github.com>
2023-04-30 18:22:42 +0200

    Add files via upload

---
+++ arrays/sort an array of strings/index.js
@@ -0,0 +1,11 @@
+let fruits = ["banana", "apple", "orange", "mango"];
+
+// fruits = fruits.sort(); // to sort by alphabeth order
+fruits = fruits.sort().reverse; // reversed alphabeth order
+
+
+
+for(let fruit of fruits)
+{
+    console.log(fruit);
+}
